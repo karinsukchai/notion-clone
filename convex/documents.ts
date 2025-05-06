92a355cec5c859e050573c6d823144eef2b1cb8f
Kan Karin <karin.s@wirtual.co>
2025-05-06 23:05:40 +0700

    feat: implement search functionality with command dialog, add search hook, and enhance navigation

--- convex/documents.ts
+++ convex/documents.ts
@@ -168,7 +168,7 @@ export const restore = mutation({
       }
     }
 
-   const document = await ctx.db.patch(args.id, options);
+    const document = await ctx.db.patch(args.id, options);
 
     recursiveRestore(args.id);
 
@@ -199,5 +199,25 @@ export const remove = mutation({
     const document = await ctx.db.delete(args.id);
 
     return document;
+  },
+});
+
+export const getSearch = query({
+  handler: async (ctx) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Not authenticated");
+    }
+    const userId = identity.subject;
+
+    const documents = await ctx.db
+      .query("documents")
+      .withIndex("by_user", (q) => q.eq("userId", userId))
+      .filter((q) => q.eq(q.field("isArchived"), false))
+      .order("desc")
+      .collect();
+
+    return documents;
   },
 });
