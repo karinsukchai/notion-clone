a32f3ee3320cbaf6c6ffa0b19ba03632afb9861c
Kan Karin <karin.s@wirtual.co>
2025-05-07 15:02:33 +0700

    feat: implement menu and banner components for document management, enhance navbar with dynamic rendering

--- convex/documents.ts
+++ convex/documents.ts
@@ -252,35 +252,40 @@ export const getById = query({
   },
 });
 
-
-export const update =mutation({
-  args:{
-    id:v.id("documents"),
+export const update = mutation({
+  args: {
+    id: v.id("documents"),
     title: v.optional(v.string()),
     content: v.optional(v.string()),
-    coverImage:v.optional(v.string()),
+    coverImage: v.optional(v.string()),
     icon: v.optional(v.string()),
-    isPublished:v.optional(v.boolean())
+    isPublished: v.optional(v.boolean()),
   },
-  handler: async(ctx,args)=>{
+  handler: async (ctx, args) => {
     const identity = await ctx.auth.getUserIdentity();
 
-    if(!identity){
+    if (!identity) {
       throw new Error("Unauthenticated");
     }
 
     const userId = identity.subject;
 
-    const {id,...rest} = args
+    const { id, ...rest } = args;
 
     const existingDocument = await ctx.db.get(args.id);
 
-    if(!existingDocument){
+    if (!existingDocument) {
       throw new Error("Not found");
     }
 
-    if(existingDocument.userId !== userId){
+    if (existingDocument.userId !== userId) {
       throw new Error("Unauthorized");
     }
-  }
-})
+
+    const document = await ctx.db.patch(args.id, {
+      ...rest,
+    });
+
+    return document;
+  },
+});
