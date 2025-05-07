e5b9747028393960fe368348c10963b6c5e4e0c7
Kan Karin <karin.s@wirtual.co>
2025-05-08 02:15:41 +0700

    feat: implement publish functionality with copy link feature and integrate into navbar

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -83,7 +83,7 @@ export const Item = ({
         if (!expanded) {
           onExpand?.();
         }
-         router.push(`/documents/${documentId}`);
+        router.push(`/documents/${documentId}`);
       }
     );
     toast.promise(promise, {
@@ -117,7 +117,7 @@ export const Item = ({
       {documentIcon ? (
         <div className="shrink-0 mr-2 text-[18px]">{documentIcon}</div>
       ) : (
-        <Icon className="shrink-0 h-[18px] mr-2 text-muted-foreground" />
+        <Icon className="shrink-0 h-[18px] w-[18px] mr-2 text-muted-foreground" />
       )}
 
       <span className="truncate">{label}</span>
