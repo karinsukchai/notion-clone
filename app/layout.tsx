06fab95cd7b53f21438205518a256cba8cab8b21
Kan Karin <karin.s@wirtual.co>
2025-05-08 11:06:57 +0700

    chore: update Next.js version to ^13.5.6 and include next.config.js in TypeScript configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,14 @@
 import { Toaster } from "sonner";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+
 import "./globals.css";
 import { ThemeProvider } from "@/components/providers/theme-provider";
 import { ConvexProvider } from "@/components/providers/convex-provider";
 import { ModalProvider } from "@/components/providers/modal-provider";
 import { EdgeStoreProvider } from '@/lib/edgestore';
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+
+
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -29,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en" className="light" style={{ colorScheme: "light" }}>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={` antialiased`}
       >
         <ConvexProvider>
         <EdgeStoreProvider>
--- convex/documents.ts
+++ convex/documents.ts
@@ -2,8 +2,7 @@ import { v } from "convex/values";
 
 import { mutation, query } from "./_generated/server";
 import { Doc, Id } from "./_generated/dataModel";
-import { error } from "console";
-import { argv } from "process";
+
 
 export const archive = mutation({
   args: {
